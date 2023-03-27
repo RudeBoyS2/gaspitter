ec7043b6d5fd1385d2dc4b56369ecb56b077470a
Gaspi <gasparelpi@gmail.com>
2023-03-27 18:25:55 -0300

    fixing chakranextimage component

--- src/components/ChakraNextImage.tsx
+++ src/components/ChakraNextImage.tsx
@@ -2,12 +2,12 @@
 import {Box} from "@chakra-ui/react";
 import Image from "next/image";
 
-const ChakraNextImage: any = ({src, alt, priority, width, height, borderRadius, ...props}: any) => {
+const ChakraNextImage: any = ({src, alt, priority, width, height, borderRadius, sizes, ...props}: any) => {
   const isPriority = priority ? true : false;
 
   return (
     <Box position="relative" width={width} height={height} borderRadius={borderRadius} {...props}>
-      <Image alt={alt} priority={isPriority} fill style={{objectFit: "cover", borderRadius: borderRadius}} quality="100" src={src} />
+      <Image alt={alt} priority={isPriority} sizes={sizes} fill={true} style={{objectFit: "cover", borderRadius: borderRadius}} quality="100" src={src} />
     </Box>
   );
 };
